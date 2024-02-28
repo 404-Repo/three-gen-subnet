e8939b93053c8f4fdb007595b047a1d120f4035f
Maxim Bladyko <bladyko@gmail.com>
2024-02-28 20:34:47 +0300

    Validation endpoint added

--- validation/validation_launcher.py
+++ validation/validation_launcher.py
@@ -1,41 +1,19 @@
-import sys
-import os
-import inspect
-
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-rootdir = os.path.dirname(parentdir)
-sys.path.insert(0, parentdir)
-sys.path.insert(0, parentdir + "/mining")
-
 import base64
-import numpy as np
-import ValidationTextTo3DModel as validation
 
-import mining.DreamGaussianLib.HDF5Loader as HDF5Loader
+from validation.ValidationLib import HDF5Loader, ValidationTextTo3DModel as validation
 from neurons.protocol import TextTo3D
 from time import time
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     loader = HDF5Loader.HDF5Loader()
 
     t1 = time()
     Validator = validation.ValidateTextTo3DModel(512, 512, 10)
 
-    data_dict = loader.load_point_cloud_from_h5(
-        "frog", "/home/tesha/Documents/Python/404_dream_gaussian/mining/logs/output7"
-    )
-    pcl_buffer = loader.pack_point_cloud_to_io_buffer(
-        data_dict["points"],
-        data_dict["normals"],
-        data_dict["features_dc"],
-        data_dict["features_rest"],
-        data_dict["opacities"],
-        data_dict["scale"],
-        data_dict["rotation"],
-        data_dict["sh_degree"],
-    )
+    data_dict = loader.load_point_cloud_from_h5("frog", "/home/tesha/Documents/Python/404_dream_gaussian/mining/logs/output7")
+    pcl_buffer = loader.pack_point_cloud_to_io_buffer(data_dict['points'], data_dict['normals'], data_dict['features_dc'], data_dict['features_rest'],
+                                                      data_dict['opacities'], data_dict['scale'], data_dict['rotation'], data_dict['sh_degree'])
 
     pcl_buffer_encode = base64.b64encode(pcl_buffer.getbuffer())
     pcl_buffer = base64.b64decode(pcl_buffer_encode)
