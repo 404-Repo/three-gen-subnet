279347dd3ba3982087b48daed0873b05b126b1e5
Ruben Hohndorf <ruben.hohndorf@gmail.com>
2024-03-15 08:26:05 +0100

    Add ficture to load tiger pcl in validator test

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,13 +1,28 @@
+import base64
+
+import h5py
+import pytest
+
 from neurons.old.protocol import TextTo3D
 from validation.lib.validators import TextTo3DModelValidator
 
 
-def test_validator():
-    prompt = ""
-    data = ""
+@pytest.fixture
+def h5_to_base64():
+    file_path = "resources/tiger_pcl.h5"
+    with open(file_path, "rb") as file:
+        content = file.read()
+        encoded_content = base64.b64encode(content)
+        encoded_content_str = encoded_content.decode("utf-8")
+        return encoded_content_str
+
+
+def test_validator(h5_to_base64):
+    prompt = "A tiger"
+    data = h5_to_base64
     t23D = TextTo3D(prompt_in=prompt, mesh_out=data)
     validator = TextTo3DModelValidator(512, 512, 10)
     validator.init_gaussian_splatting_renderer()
     scores = validator.score_response_gs_input([t23D], save_images=False, cam_rad=4)
 
-    assert True == True
+    assert scores[0] > 0.9
