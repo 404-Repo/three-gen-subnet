97a02dd723e7f76bf81a90a221ae1b8136616c58
Maxim Bladyko <bladyko@gmail.com>
2024-03-21 16:57:37 +0300

    mock validator script refined

--- neurons/mocks/mock_validator.py
+++ neurons/mocks/mock_validator.py
@@ -4,6 +4,7 @@ import typing
 import uuid
 
 import bittensor as bt
+import requests
 
 from common import protocol
 
@@ -23,7 +24,8 @@ async def main():
 
     alive_axon_uid = 1
 
-    task = protocol.TGTask(prompt="tourmaline tassel earring", task_id=str(uuid.uuid4()))
+    prompt = "tourmaline tassel earring"
+    task = protocol.TGTask(prompt=prompt, task_id=str(uuid.uuid4()))
 
     await dendrite.call(
         target_axon=metagraph.axons[alive_axon_uid],
@@ -34,6 +36,7 @@ async def main():
     bt.logging.info("Task sent")
 
     poll = protocol.TGPoll(task_id=task.task_id)
+    results = None
     while True:
         await asyncio.sleep(10)
 
@@ -49,13 +52,20 @@ async def main():
             continue
 
         if r.status == "DONE":
+            results = r.results
             with open("content_pcl.h5", "w") as f:
-                f.write(r.results)
+                f.write(results)
             bt.logging.info("Result save to `content_pcl.h5`")
             break
 
         bt.logging.info("Generation failed")
         break
+
+    if results is None:
+        return
+
+    validation = requests.post("http://127.0.0.1:8094/validate/", json={"prompt": prompt, "data": results})
+    bt.logging.info(f"Validation: {validation.json()}")
 
 
 async def get_config() -> bt.config:
