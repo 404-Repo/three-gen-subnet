2df419ed1d033d0d2b644f2fab8aacdae4cef966
tesha <tereshin.tesha@gmail.com>
2024-05-03 13:03:25 +0100

    [UPDATED] moved to tests, offline runner

--- validation/tests/offline_test_runner.py
+++ validation/tests/offline_test_runner.py
@@ -3,10 +3,8 @@ import os
 import base64
 import torch
 
-from lib.validators import TextTo3DModelValidator
-
-from lib.RenderingPipeline import Renderer
-from lib.ValidationPipeline import Validator
+from lib.rendering_pipeline import Renderer
+from lib.validation_pipeline import Validator
 
 from lib.hdf5_loader import HDF5Loader
 from time import time
@@ -33,17 +31,13 @@ def validate(data: str, prompt: str):
     validator.preload_scoring_model()
     score = validator.validate(images, prompt)
 
-    # validator = TextTo3DModelValidator(512, 512, 20)
-    # validator.init_gaussian_splatting_renderer()
-    # score = validator.score_response_gs_input(prompt, data, save_images=False, cam_rad=5)
-
     t2 = time()
     print(f"[INFO] Score: {score}")
     print(f"[INFO] Validation took: {t2 - t1} sec")
 
 
 if __name__ == '__main__':
-    h5_files = get_all_h5_file_names("h5_files")
+    h5_files = get_all_h5_file_names("../h5_files")
     print(h5_files)
     prompts = ["a peace of jewelry", "a ghost", "a turtle", "goblin with a weapon",]
 
