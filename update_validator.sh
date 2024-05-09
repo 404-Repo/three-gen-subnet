1b6deb959c53aca7b3b88b2ef6cf174ec802c685
Maxim Bladyko <bladyko@gmail.com>
2024-05-09 06:37:46 +0300

    script update

--- update_validator.sh
+++ update_validator.sh
@@ -6,13 +6,24 @@ script_dir="$(dirname "${BASH_SOURCE[0]}")"
 # Base directory for scripts
 base_dir="$script_dir"
 
+# Function to source script and check for errors
+source_script() {
+    local script_path="$1"
+    # Source the script and check if it fails
+    # shellcheck disable=SC1090
+    if ! source "$script_path"; then
+        echo "Error: Failed to source $script_path"
+        exit 1
+    fi
+}
+
 # Navigate to the neurons directory and source update script
 cd "$base_dir/neurons" || exit 1
-source update_env.sh
+source_script "update_env.sh"
 
 # Navigate to the validation directory and source update script
 cd "$base_dir/validation" || exit 1
-source update_env.sh
+source_script "update_env.sh"
 
 # Return to the base directory
 cd "$base_dir" || exit 1
