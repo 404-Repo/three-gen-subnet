09784c6aac9ddae0251a03d10caeca300faeb031
tesha <tereshin.tesha@gmail.com>
2024-05-24 17:55:58 +0100

    [UPDATED] deleting extras if installation failed

--- validation/cleanup_env.sh
+++ validation/cleanup_env.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+
+folder_path="./extras"
+
+if [ -d "$folder_path" ]; then
+    # Folder exists, delete it
+    echo "Deleting folder: $folder_path"
+    rm -rf "$folder_path"
+    echo "Folder deleted successfully."
+fi
+
 conda env remove --name three-gen-validation
+
