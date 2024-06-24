6111a3b3febf7b98efe99031954b147edbc675d8
Maxim Bladyko <bladyko@gmail.com>
2024-06-24 17:51:59 +0300

    Client example update

--- neurons/mocks/mock_client.py
+++ neurons/mocks/mock_client.py
@@ -1,19 +1,34 @@
 import asyncio
+import base64
 import json
+import re
+import unicodedata
+from pathlib import Path
 
 from aiohttp import ClientSession, WSMsgType
 from validator.api.protocol import Auth, PromptData, TaskStatus, TaskUpdate
 
 
+def normalize_filename(filename: str) -> str:
+    filename = filename.lower()
+    filename = filename.replace(" ", "_")
+    filename = unicodedata.normalize("NFKD", filename).encode("ASCII", "ignore").decode("ASCII")
+    filename = re.sub(r"[^\w\-.]", "", filename)
+    filename = filename.strip("._")
+    return filename
+
+
 async def main() -> None:
-    await asyncio.gather(*[work() for _ in range(20)])
+    await asyncio.gather(*[work() for _ in range(1)])
 
 
 async def work() -> None:
+    prompt = "Donald Duck"
+
     async with ClientSession() as session:
         async with session.ws_connect("wss://1qshev6dbe7gdz-8888.proxy.runpod.net/ws/generate/") as ws:
-            await ws.send_json(Auth(api_key="").dict())
-            await ws.send_json(PromptData(prompt="Donald Duck", send_first_results=True).dict())
+            await ws.send_json(Auth(api_key="API KEY GOES HERE").dict())
+            await ws.send_json(PromptData(prompt=prompt, send_first_results=True).dict())
 
             async for msg in ws:
                 if msg.type == WSMsgType.TEXT:
@@ -28,6 +43,11 @@ async def work() -> None:
                         score = update.results.score if update.results else None
                         assets = update.results.assets or "" if update.results else ""
                         print(f"Best results. Score: {score}. Size: {len(assets)}")
+                        print(f"Stats: {update.statistics}")
+
+                        if assets:
+                            with Path(normalize_filename(prompt) + ".ply").open("wb") as f:  # noqa
+                                f.write(base64.b64decode(assets.encode("utf-8")))
                 elif msg.type == WSMsgType.ERROR:
                     print(f"WebSocket connection closed with exception: {ws.exception()}")
 
